64dc503af82896922203c2303284bde62444060c
Brahim Hamdouni <brahim@hamdouni.com>
2023-05-18 15:07:37 +0200

    refacto db teardown

--- cmd/example/store.go
+++ cmd/example/store.go
@@ -12,20 +12,27 @@ import (
 const pragma = "_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=busy_timeout(8000)&_pragma=journal_size_limit(100000000)"
 
 // getStore returns a store and a teardown function to clean every thing before quitting
-func getStore() (dbs db.Store, teardown func(), err error) {
+func getStore() (dbs db.Store, teardown func() error, err error) {
 	tmpDir, err := os.MkdirTemp("", "temp")
 	if err != nil {
 		return dbs, teardown, fmt.Errorf("could not create a temporary folder for database: %s", err)
 	}
-	defer os.RemoveAll(tmpDir)
 
 	dbpath := filepath.Join(tmpDir, "testbase.db")
 	dbs, err = db.Open(dbpath, pragma)
 	if err != nil {
 		return dbs, teardown, fmt.Errorf("could not open database: %s", err)
 	}
-	return dbs, func() {
-		os.RemoveAll(tmpDir)
-		dbs.Close()
+	// the teardown function
+	return dbs, func() error {
+		err := dbs.Close()
+		if err != nil {
+			return fmt.Errorf("closing db: %s", err)
+		}
+		err = os.RemoveAll(tmpDir)
+		if err != nil {
+			return fmt.Errorf("removing temporary dir %s: %s", tmpDir, err)
+		}
+		return nil
 	}, nil
 }
